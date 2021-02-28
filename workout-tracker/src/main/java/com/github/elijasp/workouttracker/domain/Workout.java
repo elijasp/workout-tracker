d044b0ab7da4526cb70c97749e8bcee56c0f4c5c
elijasp <gitelijas@gmail.com>
2021-02-28 00:49:50 +0000

    Add validation for Workout entity

--- workout-tracker/src/main/java/com/github/elijasp/workouttracker/domain/Workout.java
+++ workout-tracker/src/main/java/com/github/elijasp/workouttracker/domain/Workout.java
@@ -1,6 +1,10 @@
 package com.github.elijasp.workouttracker.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
@@ -11,10 +15,14 @@ public class Workout {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+    @NotBlank(message = "Muscles worked is required")
     @Column(name = "muscles")
     private String muscles;
+    @NotBlank(message = "Description of workout is required")
     @Column(name = "description")
     private String description;
+    @NotNull(message = "DateTime of workout is required")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     @Column( name = "workout_datetime")
     private LocalDateTime workoutDateTime;
 
