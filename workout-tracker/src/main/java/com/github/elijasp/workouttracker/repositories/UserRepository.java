92ac5009e1ad76e8f4f30a566d19c4c0429b53ff
elijasp <gitelijas@gmail.com>
2021-03-03 09:58:18 +0000

    Create User Repository

---
+++ workout-tracker/src/main/java/com/github/elijasp/workouttracker/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.github.elijasp.workouttracker.repositories;
+
+import com.github.elijasp.workouttracker.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
